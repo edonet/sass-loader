4bab9d69bb7ff0736d0775ea9432887a9e2d473b
edonet <edonet@163.com>
2018-08-10 21:24:09 +0800

    update: importer

---
+++ src/default.js
@@ -0,0 +1,19 @@
+/**
+ *****************************************
+ * Created by lifx
+ * Created on 2018-08-06 13:31:49
+ *****************************************
+ */
+'use strict';
+
+import size from './fontSize';
+
+
+/**
+ *****************************************
+ * 定义样式
+ *****************************************
+ */
+export const color = '#666';
+export const borderColor = '#666';
+export const fontSize = size;
